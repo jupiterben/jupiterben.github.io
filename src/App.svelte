fe80bd771019879e704800b9c54e74549142d317
fub <jupiter.ben@live.com>
2024-08-08 19:42:10 +0800

    add sidebar

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,12 @@
 <script lang="ts">
+  import SideBar from "./SideBar.svelte";
+
 </script>
 
 <main class="m-0 h-screen bg-gray-100 flex 
 bg-[url('images/bg.jpg')] bg-center bg-cover">
 
+<SideBar/>
 
 </main>
 
--- src/SideBar.svelte
+++ src/SideBar.svelte
@@ -1,8 +1,24 @@
 <script lang="ts">
 </script>
 
-
-
-<div>
-
+<div class="float-left flex h-screen items-center justify-center w-24">
+  <div
+    class="rounded-full bg-black flex flex-col p-1 bg-white/20 backdrop-blur-md"
+  >
+    <!-- Sidebar content -->
+    <div class="m-1">
+      <img
+        src="https://avatars.githubusercontent.com/u/10271109?v=4"
+        alt="avatar"
+        class="rounded-full w-12 h-12"
+      />
+    </div>
+    <div class="m-1">
+      <img
+        src="https://avatars.githubusercontent.com/u/10271109?v=4"
+        alt="avatar"
+        class="rounded-full w-12 h-12"
+      />
+    </div>
+  </div>
 </div>
