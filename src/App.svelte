c1ce7ad073528e4259c5e9b593ca0ccb4817f997
fub <jupiter.ben@live.com>
2024-08-08 13:55:37 +0800

    add icon

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
 </script>
 
-<main class="m-0 h-screen bg-gray-100 flex"></main>
+<main class="m-0 h-screen bg-gray-100 flex 
+bg-[url('images/bg.jpg')] bg-center bg-cover">
+
+
+</main>
 
 <style>
 </style>
